d37650d08dd6ac08c30500f66015bf7b53253fe5
Palash Dhavle <palashdhavle15@gmail.com>
2023-11-22 20:47:24 +0530

    feat: added radix themes

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// radix theme css
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Theme>
+      <MyApp />
+    </Theme>
   </React.StrictMode>,
 )
