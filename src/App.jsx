9a9e6368b41d747c6e67e58333aa3645e514aba9
Palash Dhavle <palashdhavle15@gmail.com>
2023-11-22 22:02:33 +0530

    feat: created routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,31 @@ import {
   RouterProvider
 } from "react-router-dom";
 import Root from './routes/Root';
+import Home2 from './components/Home2';
 
+// initializing react router
 const router = createBrowserRouter(
   createRoutesFromElements(
+    <>
+    
     <Route
       path="/"
       element={<Root />}
       // loader={rootLoader}
       // action={rootAction}
       errorElement={<ErrorPage />}
-    >
-      <Route errorElement={<ErrorPage />} element={<Root />}/>
-
-    </Route>))
+      >
+        
+    </Route>
+    <Route
+      path="login"
+      element={<Home2 />}
+      // loader={rootLoader}
+      // action={rootAction}
+      errorElement={<ErrorPage />}
+    ></Route>
+      </>
+    ))
 
 function App() {
   return (
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Home from '../components/Home'
+import { Outlet } from 'react-router-dom'
 
 function Root() {
   return (
+    <>
     <Home/>
+    <Outlet></Outlet>
+    </>
+    
   )
 }
 
