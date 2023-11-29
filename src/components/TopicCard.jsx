35e28af7a63d9a3e8755bcb95add07b7188fee0d
Palash Dhavle <palashdhavle15@gmail.com>
2023-11-29 23:26:29 +0530

    feat: updated topic card component to include emojis for levels

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import {
     Text,
-    Card
+    Card,
 } from '@radix-ui/themes'
-function TopicCard({topic}) {
+import { Baby,Smile } from 'lucide-react'
+
+function TopicCard({ topic,level }) {
+    const levelIconMap = {
+        'AbsoluteBeginner': '🐥',
+        'Beginner': '🤓',
+        'Intermidiate': '🧐',
+        'Expert':'😎'
+    }
     return (
         <Card className='h-12 mt-2 ms-2 me-3 hover:ring-2 ring-lime-300 cursor-pointer'>
-            <Text> {topic}</Text>
+            <div className="flex justify-between">
+                <Text> {topic}</Text>
+                {levelIconMap[level]}
+            </div>
+
         </Card>
     )
 }
