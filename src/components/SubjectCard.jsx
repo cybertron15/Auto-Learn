498120c07cd6b9e5a75a02f6e4e5cfbe7b76ede2
Palash Dhavle <palashdhavle15@gmail.com>
2023-12-04 23:33:44 +0530

    feat: added trash icon for subject card

--- src/components/SubjectCard.jsx
+++ src/components/SubjectCard.jsx
@@ -4,19 +4,23 @@ import {
     Card,
 } from '@radix-ui/themes'
 import makeTitleCase from '../utils/makeTitleCase'
+import { Trash } from 'lucide-react'
 
-function SubjectCard({ topic,level }) {
+function SubjectCard({ topic, level }) {
     const levelIconMap = {
         'AbsoluteBeginner': '🐥',
         'Beginner': '🤓',
         'Intermidiate': '🧐',
-        'Expert':'😎'
+        'Expert': '😎'
     }
     return (
         <Card className='h-12 mt-2 ms-2 me-3 hover:ring-2 ring-lime-300 cursor-pointer'>
             <div className="flex justify-between">
                 <Text> {makeTitleCase(topic)}</Text>
-                {levelIconMap[level]}
+                <div className='flex gap-2'>
+                    {levelIconMap[level]}
+                    <Trash width={20} height={20} className=''/>
+                </div>
             </div>
 
         </Card>
