b3d51dedc151016b2a4941a49e21ddccbe6ce111
Palash Dhavle <palashdhavle15@gmail.com>
2023-12-07 11:35:46 +0530

    Feat: customized tailwind cnf to add borderradius property

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: { 'XXXL': '5rem' }, // added custom border radius size
+    },
   },
   plugins: [],
 }
