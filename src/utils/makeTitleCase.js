b3900e7d2f7a71e3811513600232674da3b534f5
Palash Dhavle <palashdhavle15@gmail.com>
2023-12-15 23:14:27 +0530

    feat: improved makeTitleCase util

--- src/utils/makeTitleCase.js
+++ src/utils/makeTitleCase.js
@@ -1,7 +1,12 @@
 function makeTitleCase(string) {
-    if (string)
-        return string.slice(0,1).toUpperCase() + string.slice(1,string.length).toLowerCase()
+    if (string){
+        let res = ""
+        string.split(" ").forEach(element => {
+           res += " " + element.slice(0, 1).toUpperCase() + element.slice(1, string.length).toLowerCase()
+        });
+
+        return res.trim()
+    }
     else return ""
 }
-
 export default makeTitleCase
