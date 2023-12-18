4759cc30aaa5bead40e263e833599bf447945fa3
Palash Dhavle <palashdhavle15@gmail.com>
2023-12-18 22:03:47 +0530

    added subejct route to react router

--- src/App.jsx
+++ src/App.jsx
@@ -6,33 +6,46 @@ import {
   Route,
   RouterProvider
 } from "react-router-dom";
-import Root from './routes/Root';
+import Root from './routes/Home';
 import Authenticate from './routes/Authenticate';
-import Test from './components/Test';
+// import Test from './components/Test';
+import Subject, {loader as subjectLoader} from './routes/Subject';
 
 // initializing react router
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-    
-    <Route
-      path="/"
-      element={<Root />}
-      // loader={rootLoader}
-      // action={rootAction}
-      errorElement={<ErrorPage />}
+      <Route
+        path="/"
+        element={<Root />}
+        // loader={rootLoader}
+        // action={rootAction}
+        errorElement={<ErrorPage />}
       >
-        
-    </Route>
-    <Route
-      path="login"
-      element={<Authenticate />}
-      // loader={rootLoader}
-      // action={rootAction}
-      errorElement={<ErrorPage />}
-    ></Route>
-      </>
-    ))
+        <Route
+          path="/subject/:subjectID"
+          element={<Subject />}
+          loader={subjectLoader}
+          // action={rootAction}
+          errorElement={<ErrorPage />}
+        ></Route>
+      </Route>
+      <Route
+        path="login"
+        element={<Authenticate />}
+        // loader={rootLoader}
+        // action={rootAction}
+        errorElement={<ErrorPage />}
+      ></Route>
+      {/* <Route
+        path="/test"
+        element={<Test />}
+        // loader={rootLoader}
+        // action={rootAction}
+        errorElement={<ErrorPage />}
+      ></Route> */}
+    </>
+  ))
 
 function App() {
   return (
